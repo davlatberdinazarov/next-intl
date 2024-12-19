7b5af6a178c60e218e2e05ff460eadbd36190bf5
davlatberdinazarov <aberdinazarov71@gmail.com>
2024-12-19 11:16:39 +0500

    first commit

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const t = useTranslations('IndexPage');
 
   return (
-    <div>
+    <div className='px-3'>
       <h1 className='text-4xl mb-4 font-semibold'>{t('title')}</h1>
       <p>{t('description')}</p>
     </div>
